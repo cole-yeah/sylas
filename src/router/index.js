f44c770649d4433c78c22c5b22fb66849c28c66e
chenlecheng <243461254@qq.com>
2021-04-21 20:35:56 +0800

    fix(router): 调整环境变量

--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,11 @@ import { BrowserRouter, Switch, Route } from 'lib/router';
 import Alg from 'pages/Alg';
 import Main from 'pages/Main';
 
+const isProdEnv = process.env.NODE_ENV === 'production';
+
 const Router = ({ history }) => {
-  const isDevEnv = process.env.NODE_ENV === 'development';
   return (
-    <BrowserRouter history={history} basename={isDevEnv ? '' : '/sylas'}>
+    <BrowserRouter history={history} basename={isProdEnv ? '/sylas' : ''}>
       <Switch>
         <Route path="/alg" component={Alg} />
         <Route path="/main" component={Main} />
