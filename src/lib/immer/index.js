df5a806c0b3561e1321bce93ed9d89b6552a5f85
chenlecheng <243461254@qq.com>
2021-04-22 18:25:36 +0800

    feat(immer): 实现简单immer

--- src/lib/immer/index.js
+++ src/lib/immer/index.js
@@ -1,1 +1,41 @@
-export const produce = (target, cb = () => {}) => {};
+/**
+ * base use
+ * import { produce } from 'immer'
+ * const [state, setState] = useState({a: { aa: 3 }, b: 2});
+ * const newState = produce(state, (draft) => {
+ *  draft.a.aa = 90
+ * })
+ * setState(newState)
+ */
+
+//
+const shallowCopy = data => {
+  return Array.isArray(data) ? [...data] : { ...data };
+};
+
+const createProxy = (target, opts) => new Proxy(target, opts);
+
+export const produce = (target, cb = () => {}) => {
+  let obj = {};
+  const opts = {
+    get(target, propKey) {
+      // const data = target[propKey];
+      // if (typeof data === 'object') {
+      //   createProxy(data, opts);
+      // }
+      return Reflect.get(target, propKey);
+    },
+    set(target, propKey, value) {
+      // const data = target[propKey];
+      // if (typeof data === 'object') {
+      //   createProxy(data, opts);
+      // }
+      obj = shallowCopy(target);
+      obj[propKey] = value;
+      return true;
+    },
+  };
+  const proxy = createProxy(target, opts);
+  cb(proxy);
+  return obj;
+};
--- src/pages/Alg/index.js
+++ src/pages/Alg/index.js
@@ -1,7 +1,76 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from '../../lib/router';
+import { produce } from 'lib/immer';
+
+// 循环
+const createFlow = effects => {
+  return {
+    async run(cb = () => {}) {
+      while (effects.length) {
+        const effect = effects.shift();
+        if (effect.run) {
+          effect.run();
+        } else if (Array.isArray(effect)) {
+          await createFlow(effect).run();
+        } else {
+          await effect();
+        }
+      }
+      return Promise.resolve().then(cb);
+    },
+  };
+};
+
+// 迭代
+// const createFlow = effects => {
+//   const next = async (index = 0) => {
+//     if (index === effects.length) {
+//       return Promise.resolve();
+//     }
+//     const effect = effects[index];
+//     if (effect.run) {
+//       return effect.run(() => next(index + 1));
+//     } else if (Array.isArray(effect)) {
+//       return createFlow(effect).run(() => next(index + 1));
+//     } else {
+//       return Promise.resolve(effect()).then(() => next(index + 1));
+//     }
+//   };
+//   return {
+//     async run(cb = () => {}) {
+//       return next().then(cb);
+//     },
+//   };
+// };
+
+var obj = { a: { aa: 1 }, b: 2 };
 
 const Alg = () => {
+  useEffect(() => {
+    const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+    const subFlow = createFlow([
+      () => delay(1000).then(() => console.log('c')),
+    ]);
+
+    createFlow([
+      () => console.log('a'),
+      () => console.log('b'),
+      subFlow,
+      [() => delay(2000).then(() => console.log('d')), () => console.log('e')],
+    ]).run(() => {
+      console.log('done');
+    });
+  }, []);
+
+  useEffect(() => {
+    const newObj = produce(obj, draft => (draft.a.aa = 90));
+
+    console.log('-----', obj, newObj, obj === newObj);
+  }, []);
+
+  // 需要按照 a,b,延迟1秒,c,延迟1秒,d,e, done 的顺序打印
+
   return (
     <div>
       <Link to="/main">go to main page</Link>
