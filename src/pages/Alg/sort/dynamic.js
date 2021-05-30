498529063a56db02a5d9e0d031904d2dc5f4155d
cole-yeah <243461254@qq.com>
2021-05-30 22:52:02 +0800

    feat(dynamic): 新增最少找零方法

--- src/pages/Alg/sort/dynamic.js
+++ src/pages/Alg/sort/dynamic.js
@@ -5,4 +5,31 @@
  * - 实现要反复执行来解决子问题的部分
  * - 识别并求解出基线条件
  */
-const minCoin = () => {};
+
+// 最少硬币找零
+const minCoin = (coins, amount) => {
+  const cache = [];
+  const makeChange = value => {
+    let min = [];
+    let newMin = [];
+    let newAmount = 0;
+    for (let i = 0; i < coins.length; i++) {
+      const coin = coins[i];
+      newAmount = value - coin;
+      if (newAmount >= 0) {
+        newMin = makeChange(newAmount);
+      }
+      if (
+        newAmount >= 0 &&
+        (newMin.length < minCoin.length - 1 || !minCoin.length) &&
+        (newMin.length || !newAmount)
+      ) {
+        min = [coin].concat(newMin);
+      }
+    }
+    return (cache[value] = min);
+  };
+  return makeChange(amount);
+};
+
+export default minCoin;
