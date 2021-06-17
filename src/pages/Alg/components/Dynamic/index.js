5ff6217b387aa008db7aa9387ef0fdd5eaa593fa
chenlecheng <243461254@qq.com>
2021-06-17 18:38:23 +0800

    feat(dynamic): 新增方法

--- src/pages/Alg/components/Dynamic/index.js
+++ src/pages/Alg/components/Dynamic/index.js
@@ -1,13 +1,14 @@
 import React, { useEffect } from 'react';
-import { lcs, changeCoin } from '../../dynamic';
+import { lcs, lcs_1, changeCoin, changeCoin_1 } from '../../dynamic';
 
 const Dynamic = () => {
   useEffect(() => {
     const str1 = 'ABCBDAB';
     const str2 = 'BDCABA';
-    const val = changeCoin(6);
-    // lcs(str1, str2);
-    console.log('xxxxx22222', val);
+    // const val = changeCoin(6);
+    changeCoin_1(17);
+    lcs(str1, str2);
+    lcs_1(str1, str2);
   }, []);
   return <div>dynamic</div>;
 };
--- src/pages/Alg/dynamic/index.js
+++ src/pages/Alg/dynamic/index.js
@@ -19,18 +19,31 @@ export const lcs = (str1, str2) => {
         result[i][j] = 0;
         continue;
       }
+      result[i][j] = max(result[i - 1][j], result[i][j - 1]);
       if (ary1[i] === ary2[j]) {
-        result[i][j] = result[i][j - 1] + 1;
-      } else {
-        const maxVal = max(result[i - 1][j], result[i][j - 1]);
-        result[i][j] = maxVal;
+        result[i][j] = Math.max(result[i][j], result[i - 1][j - 1] + 1);
       }
     }
   }
   return result;
 };
+export const lcs_1 = (str1, str2) => {
+  const result = Array.from({ length: str1.length + 1 }, () =>
+    Array(str2.length + 1).fill(0),
+  );
+  for (let i = 1; i <= str1.length; i++) {
+    for (let j = 1; j <= str2.length; j++) {
+      result[i][j] = Math.max(result[i - 1][j], result[i][j - 1]);
+      if (str1[i - 1] === str2[j - 1]) {
+        result[i][j] = Math.max(result[i][j], result[i - 1][j - 1] + 1);
+      }
+    }
+  }
+};
 
-// const total = 36
+/**
+ * 最少找零
+ */
 const coins = [4, 3, 1];
 export const changeCoin = amount => {
   let cache = [];
@@ -58,4 +71,47 @@ export const changeCoin = amount => {
     return (cache[val] = min);
   };
   return makeChange(amount);
+};
+
+export const changeCoin_1 = amount => {
+  const result = Array(amount + 1).fill(Infinity);
+  result[0] = 0; // 如果为0，则找零也为0
+  for (let coin of coins) {
+    for (let i = coin; i <= amount; i++) {
+      result[i] = Math.min(result[i], result[i - coin] + 1);
+    }
+  }
+};
+
+/**
+ * 背包问题
+ */
+export const bag = () => {};
+
+/**
+ * 打家劫舍
+ */
+
+/**
+ * 最长上升子序列
+ * 输入: [10,9,2,5,3,7,101,18]
+ * 输出: 4
+ * 解释: 最长的上升子序列是 [2,3,7,101]，它的长度是 4
+ */
+export const lis = ary => {
+  if (!ary.length) return 0;
+  if (ary.length === 1) return 1;
+  let maxLength = 1;
+  let curLength = 1;
+  for (let i = 0; i < ary.length - 1; i++) {
+    const cur = ary[i];
+    const next = ary[i + 1];
+    if (next > cur) {
+      curLength++;
+      maxLength = Math.max(maxLength, curLength);
+      continue;
+    }
+    curLength = 1;
+  }
+  return maxLength;
 };
