e74b1c8971ec07ab884b7a80431ec27b055c39b3
cole-yeah <243461254@qq.com>
2021-07-04 15:45:18 +0800

    feat: update

--- src/pages/Alg/components/Dynamic/index.js
+++ src/pages/Alg/components/Dynamic/index.js
@@ -1,7 +1,27 @@
 import React, { useEffect } from 'react';
-import { lcs, lcs_1, changeCoin, changeCoin_1 } from '../../dynamic';
+import { lcs, lcs_1, lcs_2, changeCoin, changeCoin_1 } from '../../dynamic';
 
-const Dynamic = () => {
+var a = 90;
+
+const obj = { a: 1 };
+
+// eslint-disable-next-line no-extend-native
+Function.prototype.myCall = function (ctx, ...rest) {
+  ctx.fn = this;
+  const reuslt = ctx.fn(...rest);
+  delete ctx.fn;
+  return reuslt;
+};
+
+function fn(n1, n2) {
+  console.log('fn console log', this, n1, n2);
+  return n1 + n2;
+}
+let a1 = fn(1, 2);
+let a2 = fn.myCall(obj, 1, 2);
+console.log('xxxxxxxxx===', a1, a2);
+
+function Dynamic() {
   useEffect(() => {
     const str1 = 'ABCBDAB';
     const str2 = 'BDCABA';
@@ -9,8 +29,10 @@ const Dynamic = () => {
     changeCoin_1(17);
     lcs(str1, str2);
     lcs_1(str1, str2);
+    lcs_2(str1, str2);
   }, []);
+  useEffect(function () {}, []);
   return <div>dynamic</div>;
-};
+}
 
 export default Dynamic;
--- src/pages/Alg/heap/minHeap.js
+++ src/pages/Alg/heap/minHeap.js
@@ -3,12 +3,104 @@
  * - 一颗完全二叉树
  * - 二叉堆不是最小堆就是最大堆
  */
+
+import { swap } from '../utils';
+
+const Compare = {
+  LESS_THAT: -1,
+  BIGGER_THAN: 1,
+};
+
+const defaultCompare = (a, b) => {
+  if (a === b) {
+    return 0;
+  }
+  return a < b ? Compare.LESS_THAT : Compare.BIGGER_THAN;
+};
+
 class MinHeap {
   constructor() {
     this.heap = [];
+    this.compare = defaultCompare;
   }
-  size() {}
-  isEmpty() {}
+  getLeftIndex(index) {
+    return 2 * index + 1;
+  }
+  getRightIndex(index) {
+    return 2 * index + 2;
+  }
+  getParentIndex(index) {
+    if (index === 0) {
+      return undefined;
+    }
+    return Math.floor((index - 1) / 2);
+  }
+  // 上移操作
+  siftUp(index) {
+    let parent = this.getParentIndex(index);
+    while (
+      index > 0 &&
+      this.compare(this.heap[parent], this.heap[index]) > Compare.BIGGER_THAN
+    ) {
+      swap(this.heap, parent, index);
+      index = parent;
+      parent = this.getParentIndex(index);
+    }
+  }
+  // 下移操作
+  siftDown(index) {
+    let element = index;
+    const left = this.getLeftIndex(index);
+    const right = this.getRightIndex(index);
+    const size = this.size();
+    if (
+      left < size &&
+      this.compare(this.heap[element], this.heap[left]) > Compare.BIGGER_THAN
+    ) {
+      element = left;
+    }
+    if (
+      right < size &&
+      this.compare(this.heap[element], this.heap[right]) > Compare.BIGGER_THAN
+    ) {
+      element = right;
+    }
+    if (index !== element) {
+      swap(this.heap, index, element);
+      this.siftDown(element);
+    }
+  }
+  // 向堆中插入一个新的值。插入成功返回true，否则返回false
+  insert(value) {
+    if (value !== null) {
+      this.heap.push(value);
+      // this.siftUp(this.heap.length - 1);
+      return true;
+    }
+    return false;
+  }
+  // 移除最大值或最小值，并返回这个值
+  extract() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    if (this.size() === 1) {
+      return this.heap.shift();
+    }
+    const removeValue = this.heap.shift();
+    this.shiftDown(0);
+    return removeValue;
+  }
+  // 返回最大值（最大堆）或返回最小值（最小堆），且不会移除这个值
+  findMinimum() {
+    return this.isEmpty ? undefined : this.heap[0];
+  }
+  size() {
+    return this.heap.length;
+  }
+  isEmpty() {
+    return this.size() === 0;
+  }
 }
 
 export default MinHeap;
